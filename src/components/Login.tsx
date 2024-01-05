4cc4c4b80bfa69d89585a1123d1e213dcd25a9b4
Mustafa <mustafahaider508@gmail.com>
2024-01-05 10:05:06 +0500

    update

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -58,7 +58,7 @@ const Login = () => {
                     setErrorEmail(false);
                     seEmail(e.target.value);
                   }}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
                 {errorEmail && (
                   <p className="text-[13px] text-red-600">Email is Required*</p>
@@ -86,7 +86,7 @@ const Login = () => {
                     setErrorPass(false);
                     sePassword(e.target.value);
                   }}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
                 {errorPass && (
                   <p className="text-[13px] text-red-600">
